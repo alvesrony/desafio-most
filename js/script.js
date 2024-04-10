2b155084ff30293248811f6da0df217b513ac1b7
alvesrony <alvesrony.m@gmail.com>
2024-04-10 01:20:16 -0300

    Adicionando lógica de fala e seção contato

--- js/script.js
+++ js/script.js
@@ -33,4 +33,86 @@ $(document).ready(function() {
         }
       });
 
+    /********************  Lógica de contato ********************/
+    $('#form-contato').submit(function(event) {
+        event.preventDefault();
+    
+        var formData = $(this).serialize();
+       
+        var queryString = formData;
+
+        // converter a string em um objeto JSON
+        function queryStringToJson(queryString) {
+            var pairs = queryString.split('&');
+            var result = {};
+            pairs.forEach(function(pair) {
+                pair = pair.split('=');
+                result[pair[0]] = decodeURIComponent(pair[1] || '');
+            });
+            return JSON.stringify(result, null, 2);
+        }
+        
+        console.log(queryStringToJson(queryString));
+        $("form")[0].reset();
+    });
+
+    // remover caracteres diferentes de números
+    $('#telefone').on('input', function() {
+        var inputValue = $(this).val();
+        var numericValue = inputValue.replace(/\D/g, '');
+        $(this).val(numericValue);
+    });
+
+    // Limitar o número de caracteres nos campos
+    $('#nome').attr('maxlength', 100);
+    $('#email').attr('maxlength', 100);
+    $('#telefone').attr('maxlength', 20);
+    $('#mensagem').attr('maxlength', 500);
+
+
+    /********************  Lógica de transcrição de texto ********************/
+    $('#testar').click(function() {
+        $('#transcript-container').removeClass('d-none');
+    });
+
+    $('#button-x').click(function() {
+        $('#transcript-container').addClass('d-none');
+        $('#transcript').text('');
+        recognition.stop();
+    });
+
+    const startButton = $('#startButton');
+    const transcriptDiv = $('#transcript');
+    let recognition;
+
+    if ('webkitSpeechRecognition' in window) {
+        recognition = new webkitSpeechRecognition();
+        recognition.lang = 'pt-BR';
+
+        recognition.onstart = function() {
+            startButton.text('Escutando...');
+        };
+
+        recognition.onend = function() {
+            startButton.text('Iniciar Reconhecimento');
+        };
+
+        recognition.onresult = function(event) {
+            const resultIndex = event.resultIndex;
+            const transcript = event.results[resultIndex][0].transcript;
+            transcriptDiv.text(transcriptDiv.text() + transcript);
+        };
+
+        startButton.click(function() {
+            if (recognition.running) {
+                recognition.stop();
+            } else {
+                recognition.start();
+            }
+        });
+    } else {
+        transcriptDiv.text('Seu navegador não suporta a API de reconhecimento de voz.');
+    }
+
+
 });
